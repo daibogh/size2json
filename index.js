d07dc01e93d775cbce7741ea1cfbd19ba6e954b9
Alexisson <ivan.a.shirokov@ya.ru>
2021-11-20 14:23:49 +0300

    makeimage

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 import express from "express";
 import multer from "multer";
 import sizeOf from "image-size";
+import sharp from "sharp";
+import fs from "fs";
 
 const app = express();
 
@@ -9,7 +11,6 @@ const img = multer({
 });
 
 app
-
   .set("view engine", "ejs")
   .set("views", "views")
   .get("/", (r) => r.res.render("./index"))
@@ -21,6 +22,15 @@ app
         height: dimensions.height,
       });
     });
+  })
+  .get("/makeimage?", (r) => {
+    const width = parseInt(r.query.width);
+    const height = parseInt(r.query.height);
+    sharp("./img/ALX_ICON.png")
+      .resize(width, height)
+      .toFile("./img/output.png", (err, info) => {
+        r.res.download("./img/output.png");
+      });
   })
   .all("/login", (r) => r.res.send("alexisson"))
   .listen(process.env.PORT || 3000, () => {

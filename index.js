de3f815e1a873a12264914eb9a358e571d4cc125
Alexisson <ivan.a.shirokov@ya.ru>
2021-12-04 14:00:28 +0300

    wordpress

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ import multer from "multer";
 import sizeOf from "image-size";
 import sharp from "sharp";
 import fs from "fs";
+import axios from "axios";
 
 const app = express();
 
@@ -31,6 +32,50 @@ app
       .toFile("./img/output.png", (err, info) => {
         r.res.download("./img/output.png");
       });
+  })
+  .all("/wordpress/", (req, res) => {
+    axios
+      .post(
+        "https://wordpress.kodaktor.ru/wp-json/jwt-auth/v1/token?username=gossjsstudent2017&password=|||123|||456"
+      )
+      .then(function (response) {
+        let config = {
+          Authorization: `Bearer ${response.data.token}`,
+        };
+        let data = {
+          title: "Ivan Shirokov",
+          content: "Иван Широков",
+          status: "publish",
+        };
+        axios
+          .get("https://wordpress.kodaktor.ru/wp-json/wp/v2/posts")
+          .then(function (r) {
+            axios
+              .post(
+                `https://wordpress.kodaktor.ru/wp-json/wp/v2/posts/`,
+                null,
+                {
+                  headers: config,
+                  params: data,
+                }
+              )
+              .then((response) => {
+                res.status(200).json(response.data.id);
+              });
+          });
+      });
+  })
+  .all("/log", (r) => {
+    console.log(r.params.data);
+    console.log(r.headers);
+  })
+  .get("/sha1", (r) => {
+    r.res.render("./sha");
+  })
+  .post("/sha1", (r) => {
+    let data = r.body.inp;
+    let data_crypto = crypto.subtle.digest("SHA-1", data);
+    console.log(data_crypto);
   })
   .all("/login", (r) => r.res.send("alexisson"))
   .listen(process.env.PORT || 3000, () => {
